8ad35ff0be5626c2e2fe64f9921931da11966235
echin6 <echin6@gmail.com>
2016-09-03 01:40:49 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import csv
+f = open('faculty.csv')
+csv_f = csv.reader(f)
+
+emails = []
+
+for i in csv_f: 
+    emails.append(i[3])
+emails_no_header = emails[1:]
+
+with open('emails.csv', 'wb') as f:
+    wr = csv.writer(f, delimiter='\n')
+    wr.writerows([emails_no_header])
